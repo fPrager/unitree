e198dbcca15dee77c3034a44dc67428d9218e605
Fabian Prager <fabian.prager@3dit.de>
2018-04-19 21:26:25 -0400

    return image in promise

--- dist/render.js
+++ dist/render.js
@@ -8,7 +8,5 @@ exports.render = undefined;
 var _loadImageSimple = require('./layer/loadImageSimple');
 
 var render = exports.render = function render() {
-    (0, _loadImageSimple.loadImageSimple)('https://s3.eu-central-1.amazonaws.com/unitree/imageOrig.png', { stretch: true }).then(function () {
-        console.log('image loaded');
-    });
+    return (0, _loadImageSimple.loadImageSimple)('https://s3.eu-central-1.amazonaws.com/unitree/imageOrig.png', { stretch: true });
 };
